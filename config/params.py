e7511de346bcad074af8407b5a608d09545995ba
Alan Zhong <1227823902@qq.com>
2022-06-11 21:31:24 +0800

    "Add new func"

---
+++ config/params.py
@@ -0,0 +1,9 @@
+# 登临点暴露点
+VIEWSHED_PATH = "ascensionpoint_generate/middle/viewshed"
+ASCENSIONLIST_SAVEPATH = "static/result/denglin"
+
+# 可达域
+RAWIMG_PATH = "static/result/reachableArea/rawImg"
+REACHABLEIMG_PATH = "static/result/reachableArea/reachableImg"
+DOMAINPOINT_PATH = "ascensionpoint_generate/middle/viewshed"
+VILLAGE_EDGE_PATH = "static/result/village_edge"
